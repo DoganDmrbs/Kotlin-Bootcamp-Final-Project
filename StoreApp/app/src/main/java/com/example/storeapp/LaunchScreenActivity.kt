c72478e6154050c3c61ae9de071fdbd67ab4973b
DoganDmrbs <55080614+DoganDmrbs@users.noreply.github.com>
2022-11-07 15:22:08 +0300

    splash

--- StoreApp/app/src/main/java/com/example/storeapp/LaunchScreenActivity.kt
+++ StoreApp/app/src/main/java/com/example/storeapp/LaunchScreenActivity.kt
@@ -1,16 +1,18 @@
 package com.example.storeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class LaunchScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
 
-        val note = findViewById<ImageView>(R.id.note)
+        val note = findViewById<ImageView>(R.id.animationView)
         note.alpha = 0f
         note.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, OnboardingActivity::class.java)
